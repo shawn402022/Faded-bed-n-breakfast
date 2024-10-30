ee4469e4b5a1be8d7470699065adb5c59b5509d5
graysonslater <graysonslater@gmail.com>
2024-10-30 16:43:39 -0400

    finished phase 2

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,8 @@ const isProduction = environment === 'production';
 //initialize express application
 const app = express();
 
+const { ValidationError } = require('sequelize');
+
 app.use(morgan('dev')); //log info about req and res
 
 app.use(cookieParser());
@@ -61,5 +63,40 @@ app.use(
 
 //This line MUST be after csurf
 app.use(routes); //connect all routes to app
+
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
+  }
+  next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
 
 module.exports = app;
