274b276f602a98afb80a574d2576732edf11fa4d
graysonslater <graysonslater@gmail.com>
2024-10-30 09:24:57 -0400

    fixed migration issue on my end, added notes

--- backend/db/models/index.js
+++ backend/db/models/index.js
@@ -1,14 +1,26 @@
-'use strict';
+// IMPORTS AND REQUIREMENTS
 
+//catching common coding errors and preventing the use of certain error-prone features
+'use strict';
+//used for reading files
 const fs = require('fs');
+//used for handling file paths
 const path = require('path');
+//Imports the Sequelize library.
 const Sequelize = require('sequelize');
+//provides information about the current Node.js process.
 const process = require('process');
+//Gets the base filename of the current file (without the directory path)\
 const basename = path.basename(__filename);
+//Sets the environment
 const env = process.env.NODE_ENV || 'development';
+//Loads the database configuration from config/database.js
 const config = require(__dirname + '/../../config/database.js')[env];
+//empty object to store database models and Sequelize instances
 const db = {};
 
+
+//SETTING UP SEQUELIZE INSTANCE/CONNECTING TO DB
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
@@ -16,6 +28,8 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
+//DYNAMICALLY LOAD ALL MODEL FILES
+//automatically read all the model files in the current directory, require them, and add them to the db object
 fs
   .readdirSync(__dirname)
   .filter(file => {
@@ -31,13 +45,16 @@ fs
     db[model.name] = model;
   });
 
+//ASSOSIATIONS/DB OBJECT
+//Set up associations between models, Add Sequelize instances to the db object for easy access throughout the application
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.sequelize = sequelize; // <--- The Sequelize instance for this connection.
+db.Sequelize = Sequelize; // <--- The Sequelize class itself.
 
+//export sequilize DB instance
 module.exports = db;
