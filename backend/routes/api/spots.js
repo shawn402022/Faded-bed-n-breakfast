341b5076f77e2a4263eb40d994bde9ce44d3a79f
graysonslater <graysonslater@gmail.com>
2024-11-19 10:12:09 -0500

    fixed create a spot

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -17,64 +17,7 @@ const { check } = require('express-validator');
 
 const { handleValidationErrors } = require('../../utils/validation');
 
-//Create A spot Middleware
-const validateCreate = [
-    check('address')
-        .exists({ checkFalsy: true })
-        .withMessage("Street address is required"),
-    check('city')
-        .exists({ checkFalsy: true })
-        .withMessage("city is required"),
-    check('state')
-        .exists({ checkFalsy: true })
-        .withMessage("state is required"),
-    check('country')
-        .exists({ checkFalsy: true })
-        .withMessage("country is required"),
-    check('lat')
-        .isLength({min: 6})
-        .withMessage("Latitude is not valid"),
-    check('lng')
-        .isLength({min: 6})
-        .withMessage("Longitude is not valid"),
-    check('description')
-        .isLength({min: 6})
-        .withMessage("Description is required"),
-    check('name')
-        .isLength({max:50})
-        .withMessage("Name must be less than 50 "),
-    check('price')
-        .exists({ checkFalsy: true })
-        .withMessage("Price per day is required"),
-    handleValidationErrors
-]
 
-//create A Spot
-router.post('/new', requireAuth, validateCreate, async (req,res) =>{
-    //grab inputs from body
-    const {address, city, state, country, lat, lng, name, description, price} = req.body;
-    //get user id
-    const ownerId = req.user.id;
-    //create new spot
-    const newSpot = await Spot.create({address, city, state, country, lat, lng, name, description, price, ownerId})
-    console.log(newSpot.city)
-    const response = {
-        id: newSpot.id,
-        ownerId: newSpot.ownerId,
-        address: newSpot.address,
-        city: newSpot.city,
-        state: newSpot.state,
-        country: newSpot.country,
-        lat: newSpot.lat,
-        lng: newSpot.lng,
-        name: newSpot.name,
-        description: newSpot.description,
-        price: newSpot.price,
-        createdAt: newSpot.createdAt,
-        updatedAt: newSpot.updatedAt
-    }
-    res.status(201).json(response)
-})
 
 //Create a Booking fom a Spot based on the Spot's id
 router.post('/:spotId/bookings', requireAuth, async (req,res) => {
@@ -496,11 +439,70 @@ router.get('/', validateQuery, async (req, res) => {
 
     const allSpots = await Spot.findAll(pagination);
 
-    res.json({
+    res.status(200).json({
         Spots: allSpots,
         page,
         size
     });
 });
+
+//Create A spot Middleware
+const validateCreate = [
+    check('address')
+        .exists({ checkFalsy: true })
+        .withMessage("Street address is required"),
+    check('city')
+        .exists({ checkFalsy: true })
+        .withMessage("city is required"),
+    check('state')
+        .exists({ checkFalsy: true })
+        .withMessage("state is required"),
+    check('country')
+        .exists({ checkFalsy: true })
+        .withMessage("country is required"),
+    check('lat')
+        .isLength({min: 6})
+        .withMessage("Latitude is not valid"),
+    check('lng')
+        .isLength({min: 6})
+        .withMessage("Longitude is not valid"),
+    check('description')
+        .isLength({min: 6})
+        .withMessage("Description is required"),
+    check('name')
+        .isLength({max:50})
+        .withMessage("Name must be less than 50 "),
+    check('price')
+        .exists({ checkFalsy: true })
+        .withMessage("Price per day is required"),
+    handleValidationErrors
+]
+
+//create A Spot
+router.post('/', requireAuth, validateCreate, async (req,res) =>{
+    //grab inputs from body
+    const {address, city, state, country, lat, lng, name, description, price} = req.body;
+    //get user id
+    const ownerId = req.user.id;
+    //create new spot
+    const newSpot = await Spot.create({address, city, state, country, lat, lng, name, description, price, ownerId})
+    console.log(newSpot.city)
+    const response = {
+        id: newSpot.id,
+        ownerId: newSpot.ownerId,
+        address: newSpot.address,
+        city: newSpot.city,
+        state: newSpot.state,
+        country: newSpot.country,
+        lat: newSpot.lat,
+        lng: newSpot.lng,
+        name: newSpot.name,
+        description: newSpot.description,
+        price: newSpot.price,
+        createdAt: newSpot.createdAt,
+        updatedAt: newSpot.updatedAt
+    }
+    res.status(201).json(response)
+})
 
 module.exports = router
