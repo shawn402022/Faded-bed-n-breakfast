b3e4c340b8c71526ba340a893aab989c6bbfc749
graysonslater <graysonslater@gmail.com>
2024-11-21 16:30:42 -0500

    altered spots seeder file

--- backend/db/seeders/20241105144056-test-spots.js
+++ backend/db/seeders/20241105144056-test-spots.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
@@ -8,39 +7,42 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await Spots.bulkCreate([
+    await queryInterface.bulkInsert(options.schema ? `${options.schema}.Spots` : 'Spots', [
       {
-        "ownerId": 1,
-        "address": "123 Disney Lane",
-        "city": "San Francisco",
-        "state": "California",
-        "country": "United States of America",
-        "lat": 37.7645358,
-        "lng": 122.4730327,
-        "name": "App Academy",
-        "description": "Place where web developers are created",
-        "price": 123,
+        ownerId: 1,
+        address: "123 Disney Lane",
+        city: "San Francisco",
+        state: "California",
+        country: "United States of America",
+        lat: 37.7645358,
+        lng: -122.4730327,
+        name: "App Academy",
+        description: "Place where web developers are created",
+        price: 123,
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        "ownerId": 1,
-        "address": "empire state",
-        "city": "new york",
-        "state": "new york",
-        "country": "United States of America",
-        "lat": 37.7645358,
-        "lng": 122.4730327,
-        "name": "App Academy",
-        "description": "Place where web developers are created",
-        "price": 123,
+        ownerId: 1,
+        address: "empire state",
+        city: "new york",
+        state: "new york",
+        country: "United States of America",
+        lat: 40.7484405,
+        lng: -73.9856644,
+        name: "Empire State Building",
+        description: "Iconic skyscraper in the heart of NYC",
+        price: 123,
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
-    ], { validate: true });
+    ], {});
   },
 
   async down (queryInterface, Sequelize) {
-    options.tableName = 'Spots';
     const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options, {
-      address: { [Op.in]: ['123 Disney Lane',"empire state"] }
+    return queryInterface.bulkDelete(options.schema ? `${options.schema}.Spots` : 'Spots', {
+      address: { [Op.in]: ['123 Disney Lane', "empire state"] }
     }, {});
   }
 };
