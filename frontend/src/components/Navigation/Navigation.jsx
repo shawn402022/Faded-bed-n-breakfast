5cbd1cddb969e45b5200efa934f161fe1cb92e29
Shawn <sgiovanni1@yahoo.com>
2024-11-26 13:06:20 -0500

    finished phase 3

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,47 +1,34 @@
-// frontend/src/components/Navigation/Navigation.jsx
 
 import { NavLink } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import * as sessionActions from '../../store/session';
 
 function Navigation({ isLoaded }) {
-  const sessionUser = useSelector(state => state.session.user);
-  const dispatch = useDispatch();
+    const sessionUser = useSelector(state => state.session.user);
 
-  const logout = (e) => {
-    e.preventDefault();
-    dispatch(sessionActions.logout());
-  };
+    const sessionLinks = sessionUser ? (
+        <li>
+            <ProfileButton user={sessionUser} />
+        </li>
+    ) : (
+        <div>
+            <li>
+                <NavLink to="/login">Log In</NavLink>
+            </li>
+            <li>
+                <NavLink to="/signup">Sign Up</NavLink>
+            </li>
+        </div>
+    );
 
-  const sessionLinks = sessionUser ? (
-    <div>
-      <li>
-        <ProfileButton user={sessionUser} />
-      </li>
-      <li>
-        <button onClick={logout}>Log Out</button>
-      </li>
-    </div>
-  ) : (
-    <div>
-      <li>
-        <NavLink to="/login">Log In</NavLink>
-      </li>
-      <li>
-        <NavLink to="/signup">Sign Up</NavLink>
-      </li>
-    </div>
-  );
-
-  return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
-      {isLoaded && sessionLinks}
-    </ul>
-  );
+    return (
+        <ul>
+            <li>
+                <NavLink to="/">Home</NavLink>
+            </li>
+            {isLoaded && sessionLinks}
+        </ul>
+    );
 }
 
 export default Navigation;
--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,12 +1,56 @@
 
-import { HiMiniUser } from "react-icons/hi2";
+import { useState, useEffect, useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import { HiMiniUser } from 'react-icons/hi2';
+import * as sessionActions from '../../store/session';
+
+function ProfileButton({ user }) {
+    const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
+    const ulRef = useRef();
+
+    const toggleMenu = (e) => {
+        e.stopPropagation(); // Keep click from bubbling up to document and triggering closeMenu
+        setShowMenu(!showMenu);
+    };
+
+    useEffect(() => {
+        if (!showMenu) return;
+
+        const closeMenu = (e) => {
+            if (ulRef.current && !ulRef.current.contains(e.target)) {
+                setShowMenu(false);
+            }
+        };
+
+        document.addEventListener('click', closeMenu);
+
+        return () => document.removeEventListener('click', closeMenu);
+    }, [showMenu])
 
 
-const ProfileButton = () => {
+    const logout = (e) => {
+        e.preventDefault();
+        dispatch(sessionActions.logout());
+    };
+
+    const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
     return (
-        <div >
-            <HiMiniUser />
+        <div>
+            <button onClick={toggleMenu}>
+                <HiMiniUser />
+            </button>
+            <ul className={ulClassName} ref={ulRef}>
+                <li>{user.username}</li>
+                <li>{user.firstName} {user.lastName}</li>
+                <li>{user.email}</li>
+                <li>
+                    <button onClick={logout}>Log Out</button>
+                </li>
+            </ul>
         </div>
-    )
+    );
 }
+
 export default ProfileButton;
