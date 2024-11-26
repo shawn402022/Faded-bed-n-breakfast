21cc61d16dc9f98232f4208573a8996649202b20
Shawn <sgiovanni1@yahoo.com>
2024-11-25 19:23:04 -0500

    finished phase 1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,25 +5,30 @@ import App from './App';
 import './index.css';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
+import { RouterProvider } from 'react-router-dom';
 
 const store = configureStore();
-/*
+
 if (process.env.NODE_ENV !== 'production') {
   window.store = store;
 }
-  */
+
 
 if (import.meta.env.MODE !== 'production') {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+
+      <Provider store={store}>
+        <App />
+      </Provider>
+    
   </React.StrictMode>
 );
